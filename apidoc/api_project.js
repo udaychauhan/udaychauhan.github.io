4250efb4bfa4dc8f679a565e8583a136ece37c1a
udaychauhan <uday.myplanet@gmail.com>
2018-05-22 17:36:18 +0530

    updated api doc

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -3,13 +3,13 @@ define({
   "version": "0.0.1",
   "description": "Documentation for ChatRoomApp",
   "title": "Chat Room App APIs",
-  "url": "http://localhost:3000",
+  "url": "http://chatapi.udaychauhan.info",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-05-13T16:35:29.107Z",
+    "time": "2018-05-22T08:54:02.030Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
